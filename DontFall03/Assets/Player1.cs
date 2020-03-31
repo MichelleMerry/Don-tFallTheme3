48d8addb8bae2060c948415b598967870e8d9f02
DropBear25 <60543506+DropBear25@users.noreply.github.com>
2020-03-31 15:15:08 +0100

    game

---
+++ DontFall03/Assets/Player1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float mSpeed;
+
+    Vector3 mvelocity;
+
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        mvelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            mvelocity.z = -1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+           
+            mvelocity.z = 1.0f;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mvelocity.x = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            mvelocity.x = 1.0f;
+        }
+        transform.Translate(mvelocity.normalized * Time.deltaTime * mSpeed);
+
+
+
+    }
+}
